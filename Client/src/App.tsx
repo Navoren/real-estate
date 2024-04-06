5e297f701659af60f1244dd04211d6d2f04c3e47
Naman Tomar <namantomar1453@gmail.com>
2024-04-06 18:42:51 +0530

    init

---
+++ Client/src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
