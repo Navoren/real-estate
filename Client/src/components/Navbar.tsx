6b0ac8755a38fb6bb4868a9b6592beca2aa7beab
Naman Tomar <namantomar1453@gmail.com>
2024-04-30 03:07:18 +0530

    Add react-icons and swiper dependencies to package.json and package-lock.json

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,24 +1,25 @@
-import { NavLink } from 'react-router-dom';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Navbar() {
   const { currentUser } = useSelector((state: any) => state.user);
 
   return (
-    <div className="navbar bg-base-100 drop-shadow-sm">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+    <div className="navbar bg-base-100 drop-shadow-sm z-40">
+      <div className="navbar-start z-40">
+        <div className="dropdown z-40">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle z-40">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-20 p-2 shadow bg-base-100 rounded-box w-52">
             <li><NavLink to={'/'}>Home</NavLink></li>
             <li><NavLink to={'/about'}>About</NavLink></li>
           </ul>
         </div>
       </div>
       <div className="navbar-center">
-        <a className="font-semibold text-xl">Estate4U</a>
+        <Link to='/' className="font-semibold text-xl">Estate4U</Link>
       </div>
       <div className="navbar-end flex-none gap-2">
         <input type="text" placeholder="Search" className="input input-bordered w-12 md:w-auto" />
--- Client/src/pages/Listing.tsx
+++ Client/src/pages/Listing.tsx
@@ -1,25 +1,137 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
 
+import {
+    FaBath,
+    FaBed,
+    FaChair,
+    FaMapMarkedAlt,
+    FaMapMarkerAlt,
+    FaParking,
+    FaShare,
+} from 'react-icons/fa';
+  
 function Listing() {
-    const params = useParams();
-    const [listing, setListing] = useState(true);
-    useEffect(() => {
-        const fetchListing = async () => {
-            try {
-                const res = await fetch(`http://localhost:4000/api/v1/listing/get/${params.listingId}`)
-                const data = await res.json()
-                console.log(data)
-            } catch (error) {
-                console.error('Error fetching listing:', error)
-            }
+const params = useParams();
+const [listing, setListing] = useState<any>(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false);
+const [copied, setCopied] = useState(false);
+const [contact, setContact] = useState(false);
+useEffect(() => {
+    const fetchListing = async () => {
+        try {
+        setLoading(true);
+        const res = await fetch(`http://localhost:4000/api/v1/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+            setError(true);
+            setLoading(false);
+            return;
         }
-        fetchListing();
-    });
-  return (
-    <div>Listing</div>
-  )
+        setListing(data);
+        setLoading(false);
+        setError(false);
+        } catch (error) {
+        setError(true);
+        setLoading(false);
+        }
+    };
+    fetchListing();
+    }, [params.listingId]);
+return (
+    <main>
+        {loading ? (
+    <p className='text-center text-teal-600 font-semibold'>Loading...</p>
+    ) : error ? (
+    <p className='text-center text-red-600 font-semibold'>Error fetching listing</p>
+            ) : listing ? (
+                    <div className='place-self-cente'>
+                        <Swiper className='z-10' navigation>
+                            {listing.imageUrls.map((url: any) => (
+                                <SwiperSlide key={url}>
+                                    <div
+                                        className='h-[550px]'
+                                        style={{ background: `url(${url}) center no-repeat`, backgroundColor: 'beige', backgroundSize: 'cover' }}>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                        <div className=''>
+                        <div className='p-3'>
+                            <h1 className='text-3xl font-bold m-1'>{listing.name} - {listing.regularPrice} ($/month)</h1>
+                            <p className='text-xl font-light text-gray-500 flex mt-2'>
+                                <FaMapMarkerAlt className='text-green-700 m-1' />
+                                    {listing.address}</p>
+                                <div className='absolute top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
+                                    <FaShare
+                                        className='text-slate-500'
+                                        onClick={() => {
+                                            navigator.clipboard.writeText(window.location.href);
+                                            setCopied(true);
+                                            setTimeout(() => {
+                                                setCopied(false);
+                                            }, 2000);
+                                        }}
+                                    />
+                                </div>
+                        </div>
+                        <div className='flex gap-4 p-3'>
+                            <p className='badge badge-error font-semibold '>
+                                {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                            </p>
+                            {listing.offer && (
+                                <p className='badge badge-primary font-semibold '>
+                                    ${+listing.regularPrice - +listing.discountPrice} OFF
+                                </p>
+                            )}
+                        </div>
+                        <div>
+                        </div>
+                        <div className='p-4'>
+                            <h1 className='text-3xl font-semibold'>Description</h1>
+                            <p className='font-light'>{listing.description}</p>
+                        </div>
+                        <div className='px-4'>
+                            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6'>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaBed className='text-lg' />
+                                    {listing.bedrooms > 1
+                                        ? `${listing.bedrooms} beds `
+                                        : `${listing.bedrooms} bed `}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaBath className='text-lg' />
+                                    {listing.bathrooms > 1
+                                        ? `${listing.bathrooms} baths `
+                                        : `${listing.bathrooms} bath `}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaParking className='text-lg' />
+                                    {listing.parking ? 'Parking spot' : 'No Parking'}
+                                </li>
+                                <li className='flex items-center gap-1 whitespace-nowrap '>
+                                    <FaChair className='text-lg' />
+                                    {listing.furnished ? 'Furnished' : 'Unfurnished'}
+                                </li>
+                            </ul>
+                        </div>
+                        </div>
+                    </div>
+                ) : (
+            <p>No listing data found</p>
+        )}
+    </main>
+)
 }
 
 export default Listing
+
+
